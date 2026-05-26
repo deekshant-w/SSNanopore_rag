79c36f9d7777d6585d028bbe2ad6fa1582205df6
Deekshant Wadhwa <dwadh045@uottawa.ca>
2026-05-26 14:01:11 -0400

    Initial commit (Data loading and embedding generation)

---
+++ dataLoader.py
@@ -0,0 +1,136 @@
+from typing import Any
+from pathlib import Path
+from pydantic import BaseModel, ConfigDict
+from dotenv import load_dotenv
+from tqdm.auto import tqdm
+import json
+import re
+import logging
+
+PROJECT_DIR = Path(__file__).parent
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+
+def parse_ris_data(ris_file: Path) -> list[dict[str, str]]:
+    """
+    Parse a RIS file and return a list of dictionaries for all key value pairs.
+    The list is sorted but not separated into entries.
+
+    Args:
+        ris_file: Path to the RIS file
+
+    Returns:
+        List of dictionaries, where each dictionary contains a 'key' and 'value' field.
+        The 'key' is the field type (e.g., 'TI') and the 'value' is the field content.
+    """
+    entries: list[dict[str, str]] = []
+
+    def process_content(content):
+        content = content.strip()
+        if not content:
+            return
+        try:
+            key, value = content.split("  - ", 1)
+        except ValueError:
+            logger.warning(f"Failed to parse content: {content}")
+            return
+        entries.append({"key": key.strip(), "value": value.strip()})
+
+    with open(ris_file, "r", encoding="utf-8") as file:
+        lineContent = ""
+        for line in file:
+            # if line is of the type <capital char><capital char><space><space><-><space><text>:
+            match = re.match(r"^[A-Z][A-Z0-9]  -", line)
+            if match:
+                # process the previous linecontent
+                process_content(lineContent)
+                lineContent = line.strip()
+            else:
+                lineContent += line.strip() + " "
+        process_content(lineContent)
+    return entries
+
+
+def check_keys(data: list[dict[str, str]]):
+    from collections import Counter
+
+    keys = Counter()
+    for item in data:
+        # print(item)
+        keys.update([item["key"]])
+    print(keys)
+    exit()
+
+
+class Paper(BaseModel):
+    model_config = ConfigDict(extra="allow")
+
+    def process(self) -> dict[str, Any]:
+        result = {}
+        for key, value in self.model_dump().items():
+            if not value.strip():
+                continue
+            match key:
+                case "TI":
+                    result["title"] = value
+                case "AU":
+                    result["authors"] = result.get("authors", []) + [value]
+                case "AB":
+                    result["abstract"] = value
+                case "KW":
+                    result["keywords"] = result.get("keywords", []) + [value]
+                case "TY":
+                    result["type of reference"] = value
+                case "DO":
+                    result["doi"] = value
+                case "UR":
+                    result["url"] = value
+                case "PB":
+                    result["publisher"] = value
+                case "DA":
+                    result["date"] = value
+        return result
+
+
+def convert_ris_data_to_entities(data: list[dict[str, str]]):
+    findStage = True  # True -> Find TI
+    papers: list[dict[str, Any]] = []
+    paper = Paper()
+    i = 0
+    while i < len(data):
+        if findStage:
+            if data[i]["key"] == "TI":
+                findStage = False
+                setattr(paper, data[i]["key"], data[i]["value"])
+                i += 1
+                continue
+            else:
+                i += 1
+                continue
+        else:
+            if data[i]["key"] == "TI":
+                papers.append(paper.process())
+                paper = Paper()
+                findStage = True
+                continue
+            else:
+                setattr(paper, data[i]["key"], data[i]["value"])
+                i += 1
+    papers.append(paper.process())
+    return papers
+
+
+def make_data():
+    dataPath = PROJECT_DIR / "data"
+    ris_file = dataPath / "kyle_export.ris"
+    data = parse_ris_data(ris_file)
+    # check_keys(data)
+    papers = convert_ris_data_to_entities(data)
+    json.dump(papers, open(dataPath / "papers.json", "w", encoding="utf-8"), indent=2)
+
+
+if __name__ == "__main__":
+    make_data()
---
+++ embeddings.py
@@ -0,0 +1,121 @@
+from google.genai._interactions.types import code_execution_result_step_param
+from transformers import AutoTokenizer
+from adapters import AutoAdapterModel
+from abc import ABCMeta, abstractmethod
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+
+
+class EmbeddingService(metaclass=ABCMeta):
+    def __init__(self):
+        self.initializeModelRequirements()
+
+    @abstractmethod
+    def initializeModelRequirements(self):
+        pass
+
+    @abstractmethod
+    def getEmbeddings(self, queries: list[str]) -> list[list[float]]:
+        pass
+
+
+class BioBERT(EmbeddingService):
+    def __init__(self):
+        logger.info("Initializing BioBERT")
+        super().__init__()
+
+    def initializeModelRequirements(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-v1.1")
+        self.model = AutoAdapterModel.from_pretrained("dmis-lab/biobert-v1.1")
+
+    def getEmbeddings(self, queries: list[str]) -> list[list[float]]:
+        embeddings = []
+        for query in queries:
+            inputs = self.tokenizer(
+                query,
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=512,
+            )
+            outputs = self.model(**inputs)
+            embeddings.append(outputs.last_hidden_state[0, 0].detach().numpy().tolist())
+        return embeddings
+
+
+class Specter2(EmbeddingService):
+    def __init__(self):
+        logger.info("Initializing Specter2")
+        super().__init__()
+
+    def initializeModelRequirements(self):
+        self.tokenizer = AutoTokenizer.from_pretrained("allenai/specter2_base")
+        self.model = AutoAdapterModel.from_pretrained("allenai/specter2_base")
+        self.model.load_adapter(
+            "allenai/specter2", source="hf", load_as="specter2", set_active=True
+        )
+        self.model.set_active_adapters("specter2")
+        self.model.eval()
+
+    def getEmbeddings(self, queries: list[str]) -> list[list[float]]:
+        embeddings = []
+        for query in queries:
+            inputs = self.tokenizer(
+                query,
+                return_tensors="pt",
+                padding=True,
+                truncation=True,
+                max_length=512,
+            )
+            outputs = self.model(**inputs)
+            embeddings.append(outputs.last_hidden_state[0, 0].detach().numpy().tolist())
+        return embeddings
+
+
+class GoogleEmbeddings(EmbeddingService):
+    def __init__(self):
+        logger.info("Initializing GoogleEmbeddings")
+        super().__init__()
+
+    def initializeModelRequirements(self):
+        self.model = genai.Client()
+        self.model_name = "gemini-embedding-2"
+        self.output_dimensionality = 128  # None
+
+    def getEmbeddings(self, queries: list[str]) -> list[list[float]]:
+        embeddings = []
+        for query in queries:
+            embeddings.append(
+                self.model.models.embed_content(
+                    model=self.model_name,
+                    contents=query,
+                    config=types.EmbedContentConfig(
+                        output_dimensionality=self.output_dimensionality
+                    ),
+                )
+                .embeddings[0]
+                .values
+            )
+        return embeddings
+
+
+def main():
+    # embeddingService = GoogleEmbeddings()
+    embeddingService = BioBERT()
+    # embeddingService = Specter2()
+    embeddings = embeddingService.getEmbeddings(
+        ["what is DNA sequencing?", "what is CRISPR?"]
+    )
+    for i, embedding in enumerate(embeddings):
+        print(f"Embedding for query {i}: {embedding}")
+
+
+if __name__ == "__main__":
+    main()
